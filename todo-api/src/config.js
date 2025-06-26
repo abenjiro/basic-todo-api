1f34cbd2d4dbb924929ce6f6a14cee22f8cf8f61
jiro <bacheampong@itconsortiumgh.com>
2025-06-26 12:06:47 +0000

    code clean up

--- todo-api/src/config.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'mssql'});
-
-  export {sequelize};
--- todo-api/src/task/routes.js
+++
@@ -1,4 +0,0 @@
-const router = require('express').Router();
-
-//create new task
-router.post('/create' )
